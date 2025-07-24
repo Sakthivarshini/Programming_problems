2862e8e594a06e9ad5a24884ac1cf178ad5b74a5
Sakthivarshini <70019608+Sakthivarshini@users.noreply.github.com>
2025-07-24 20:36:44 +0530

    Update Recursive_factorial.py

--- Recursive_factorial.py
+++ Recursive_factorial.py
@@ -1,8 +1,10 @@
-def factorial(num):
-    if num <=1:
+def factorial(num, memo={}):
+    if num<=1:
         return 1
-    return num * factorial(num - 1)
-
+    if num in memo:
+        return memo[num]
+    memo[num] = num * factorial(num-1,memo)
+    return memo[num]
 n=int(input())
 result = factorial(n)
-print(result)
+print(factorial(n))
